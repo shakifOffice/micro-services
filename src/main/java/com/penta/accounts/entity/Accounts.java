4ce8ded14b5e33f5fa8a7d132e58799ad4b27c8b
Md. Shakif Sahriar <test@test.test6>
2024-04-28 11:50:54 +0600

    added entities & repository

---
+++ src/main/java/com/penta/accounts/entity/Accounts.java
@@ -0,0 +1,22 @@
+package com.penta.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Accounts extends BaseEntity{
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Id
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+}
---
+++ src/main/java/com/penta/accounts/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.penta.accounts.repository;
+
+import com.penta.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+}
